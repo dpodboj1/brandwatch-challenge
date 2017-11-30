a7f3aba8cac074ecdf218d66bfff54698ad64571
dpodboj1 <podbojec.damir@gmail.com>
2017-11-30 21:38:17 +0100

    Added prop-types to project and defined prop type for topics

--- src/TopicsCloud.js
+++ src/TopicsCloud.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TopicsCloud extends Component {
   render() {
+    const { topics } = this.props;
     return 'topics will be mapped here';
   }
 }
+
+TopicsCloud.propTypes = {
+  topics: PropTypes.array.isRequired,
+};
 
 export default TopicsCloud;
