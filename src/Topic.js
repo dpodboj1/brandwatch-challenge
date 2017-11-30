392376c95a332880583e7746c6dda68ca1fb4217
dpodboj1 <podbojec.damir@gmail.com>
2017-11-30 21:46:06 +0100

    Topic is now rendering all the topics from topics.json

--- src/Topic.js
+++ src/Topic.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Topic extends Component {
   render() {
     return (
-      <div>This is a topic.</div>
+      <div>{this.props.label}</div>
     );
   }
 }
+
+Topic.propTypes = {
+  label: PropTypes.string.isRequired,
+};
 
 export default Topic;
