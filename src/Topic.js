b59a5f45eb7fc1b3c85ffaf2b9ed03b9a3d4f999
dpodboj1 <podbojec.damir@gmail.com>
2017-11-30 22:03:03 +0100

    Added click logic to Topic and TopicsCloud

--- src/Topic.js
+++ src/Topic.js
@@ -4,12 +4,17 @@ import PropTypes from 'prop-types';
 class Topic extends Component {
   render() {
     return (
-      <div>{this.props.label}</div>
+      <div
+        onClick={this.props.click.bind(null, this.props.id)}>
+        {this.props.label}
+      </div>
     );
   }
 }
 
 Topic.propTypes = {
+  click: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
 };
 
--- src/TopicsCloud.js
+++ src/TopicsCloud.js
@@ -3,6 +3,19 @@ import PropTypes from 'prop-types';
 import Topic from './Topic';
 
 class TopicsCloud extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      currentTopicId: '',
+    };
+  }
+
+  handleClick(currentTopicId) {
+    console.log('currentTopicId is:', currentTopicId)
+    this.setState({ currentTopicId });
+  }
+
   render() {
     const { topics } = this.props;
     return (
@@ -11,6 +24,7 @@ class TopicsCloud extends Component {
           //  map through the topics array and for each object render a Topic component
           topics.map(topic =>
           (<Topic
+            click={this.handleClick}
             key={topic.id}
             {...topic}
           />))
