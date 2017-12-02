ee5cd861386fafa171a00af6b7c198bfa1fb0630
dpodboj1 <podbojec.damir@gmail.com>
2017-12-02 16:44:15 +0100

    Added normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'normalize.css';
 import App from './App';
 import './index.css';
+
 
 render(<App />, document.getElementById('root'));
