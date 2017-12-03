976f70236baa56c7c38df45d69d20824be4a932d
dpodboj1 <podbojec.damir@gmail.com>
2017-12-03 18:26:45 +0100

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="Header--container">
+    <h1>My Topics Challenge!</h1>
+  </header>
+);
+
+export default Header;
